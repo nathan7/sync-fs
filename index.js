f12020d46bdaca691152d7f415870be816f10e70
Nathan Zadoks <nathan@nathan7.eu>
2013-12-25 15:05:12 +0100

    fix everything

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 var fs = require('fs')
 
 for (var key in fs)
-  if (!(typeof fs[key] != 'function'
-      || key.match(/^[A-Z]/)
-      || key.match(/^create/) 
-      || key.match(/^(un)?watch/)
-      || key.match(/Sync$/)
-      ))
+  if (( typeof fs[key] != 'function'
+     || key.match(/^[A-Z]/)
+     || key.match(/^create/) 
+     || key.match(/^(un)?watch/)
+     || key.match(/Sync$/)
+     ))
     exports[key.replace(/Sync$/, '')] = fs[key]
